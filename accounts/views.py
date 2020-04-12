618be48e517bd9e6183b8e939d52414fc6347cc5
Jinay98 <jinayparekh98@gmail.com>
2020-04-12 19:26:57 +0530

    Adding Backend Project

---
+++ accounts/views.py
@@ -0,0 +1,90 @@
+from django.shortcuts import render, HttpResponse, redirect
+from accounts.forms import RegistrationForm, EditProfileForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from accounts.models import Patient
+from django.urls import reverse
+
+
+# Create your views here.
+# @login_required 
+def home(request):
+    numbers = [1, 2, 3, 4, 5]
+    name = 'Jinay'
+    args = {'name': name, 'numbers': numbers}
+
+    submit_button = request.POST.get('Submit')
+    if submit_button == 'Submit':
+        obj = Patient(name=request.POST.get('name'), age=int(request.POST.get('age')),
+                      gender=request.POST.get('gender'))
+        obj.save()
+
+    return render(request, 'accounts/home.html', args)
+
+
+def get_patient_details(request):
+    patients = Patient.objects.all()
+    list_of_patients = []
+    for patient in patients:
+        patient_dict = {}
+        patient_dict["id"] = patient.id
+        patient_dict["name"] = patient.name
+        patient_dict["age"] = patient.age
+        patient_dict["gender"] = patient.gender
+        list_of_patients.append(patient_dict)
+
+    return render(request, 'accounts/get_patient_details.html', {"list_of_patients": list_of_patients})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('accounts:home'))
+    else:
+        form = RegistrationForm()
+
+        args = {'form': form}
+        return render(request, 'accounts/reg_form.html', args)
+
+
+# @login_required
+def view_profile(request):
+    args = {'user': request.user}
+    return render(request, 'accounts/profile.html', args)
+
+
+# @login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('/account/profile')
+
+    else:
+        form = EditProfileForm(instance=request.user)
+
+        args = {'form': form}
+        return render(request, 'accounts/edit_profile.html', args)
+
+
+# @login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(data=request.POST, user=request.user)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return redirect('/account/profile')
+        else:
+            return redirect('/account/change-password')
+
+    else:
+        form = PasswordChangeForm(user=request.user)
+
+        args = {'form': form}
+        return render(request, 'accounts/change_password.html', args)
